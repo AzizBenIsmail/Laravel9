7d3a4135e50e533fe921404a3a6477bc91595b5f
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-09-20 16:49:31 +0100

    Controller et route
    Routing simple , Route de base , Routing paramétrés ,Routing paramétrés optionnel , Contraintes sur les lettres

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route de base
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Routing simple
+Route::get('/page1', function () {
+    echo "<h1>my first page</h1>";
+});
+
+//Routing paramétrés
+Route::get('/message/{foulen}/{CIN}', function ($foulen,$CIN) {
+    echo "<h1>Welcom $foulen CIN $CIN</h1>";
+});
+
+//Routing paramétrés optionnel
+Route::get('/message/{foulen?}/{CIN?}', function ($foulen = null ,$CIN = null) { //valeur par defaut pour eviter erreur
+    if($foulen == null )
+    {
+        $foulen = "'vide'";
+    }
+
+    if($CIN == null )
+    {
+        $CIN = "'vide'";
+    }
+    echo "<h1>Welcom $foulen CIN $CIN</h1>";
+});
+
+//Contraintes sur les lettres
+Route::get('/message1/{foulen}/{CIN}/{Identifiant}', function ($foulen, $CIN,$Identifiant) {
+    echo "<h1>Welcome : $foulen | CIN : $CIN | Identifiant : $Identifiant</h1>";
+})->where('CIN', '[0-9]{8}')->where('foulen', '[A-Za-z]+')->where('Identifiant', '[A-Za-z0-9]{10}');
+/*)->where('CIN', '\w{9}');*/  //word
+/*->where('CIN', '\d{9}');*/        //decimal
+/*->where('username', '[A-Za-z]+');*/
+
+
+//Route de base
+Route::get('/homeController/view',[HomeController::class,'afficher_une_page']);
+
+Route::get('/homeController/data',[HomeController::class,'envoyer_des_données']);
+
+Route::get('/homeController/{message}', [HomeController::class,'show']);
